2b3df15ce9d75c4b17f00e5965fd6eab9e8363df
binsoy69 <binslegendary@gmail.com>
2025-08-30 09:44:02 +0800

    ascii_challenge

---
+++ ascii.py
@@ -0,0 +1,3 @@
+ascii_values = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+ascii_characters = [chr(value) for value in ascii_values]
+print(''.join(ascii_characters))
