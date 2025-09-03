cb1cdc9f1ab97a000db6e459075b2602fb2ee7e8
binsoy69 <binslegendary@gmail.com>
2025-09-03 20:04:42 +0800

    xorkey correct answer

--- xorkey1.py
+++ xorkey1.py
@@ -1,7 +1,7 @@
 from pwn import xor
 puzzle_hex = "0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104"
 puzzle_bytes = bytes.fromhex(puzzle_hex)
-secret = xor(puzzle_bytes, b'crypto{}') # hint from challenge
+secret = xor(puzzle_bytes, b'crypto{') # hint from challenge
 print(secret.decode('utf-8'))
 res = xor(puzzle_bytes, b'myXORkey')
 print(res.decode('utf-8'))
